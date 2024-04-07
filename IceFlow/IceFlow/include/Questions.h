7c75a29d746722db4174ea790e9709c10dd5bd2f
IPVoynov22 <IPVoynov22@codingburgas.bg>
2024-04-07 19:51:18 +0300

    Questions arrays and maps

---
+++ IceFlow/IceFlow/include/Questions.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+#include <map>
+using namespace std;
+
+extern map<int, const char*> QuestionsGeography;
+
+extern map<int, const char*> QuestionsEnglish;
+
+extern map<int, const char*> QuestionsMath;
+
+extern string AnswersGeography[50][4];
+
+extern string AnswersMath[50][4];
+
+extern string AnswersEnglish[50][4];
